b3bd5c7f8a85c3d836ad60b0f70579032cc3c794
hoanglong304 <longhoang.30042000@gmail.com>
2025-04-24 16:36:56 +0700

    Initial commit

---
+++ managershop/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using managershop.Data; // Thêm namespace chứa DbContext
+using managershop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace managershop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;  // Tiêm DbContext vào Controller
+
+        // Constructor để tiêm ILogger và AppDbContext vào controller
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
+        {
+            _logger = logger;
+            _context = context;  // Lưu đối tượng AppDbContext
+        }
+
+        public IActionResult Index()
+        {
+            // Lấy danh sách sản phẩm từ cơ sở dữ liệu
+            var products = _context.Products.ToList(); // Truy vấn tất cả sản phẩm từ cơ sở dữ liệu
+
+            // Truyền danh sách sản phẩm vào View
+            return View(products);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ managershop/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using managershop.Models;  // Đảm bảo bạn đã import các model của mình
+
+namespace managershop.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // Constructor để khởi tạo DbContext với các tùy chọn cấu hình
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        { }
+
+        // Định nghĩa các DbSet để đại diện cho các bảng trong cơ sở dữ liệu
+        public DbSet<Product> Products { get; set; }
+    }
+}
